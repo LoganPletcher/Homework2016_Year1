76d951234aff5e7bc9a500f8cca3d1c00265e333
LoganPletcher <lpletch95@hotmail.com>
2016-03-22 14:47:44 -0500

    Modified script for serialization.

--- CLass Practice/CLass Practice/XML_Serialization.cs
+++ CLass Practice/CLass Practice/XML_Serialization.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace CLass_Practice
 {
-    class XML_Serialization
+    public class XML_Serialization<T>
     {
+        public XML_Serialization() { }
+
+        public void Serialize(string s, T t)
+        {
+            File.Create(@"..\..\SavedTeamFiles" + s + ".bin");
+        }
+
+        public void /*T*/ Deserialization(string s)
+        {
+
+        }
     }
 }
