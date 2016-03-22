0667b7839c8f55d751e99c69935596049a4ff83e
LoganPletcher <lpletch95@hotmail.com>
2016-03-22 15:01:27 -0500

    Modified script for saving and loading. Saving and loading script is the serialization script.

---
+++ CLass Practice/CLass Practice/Save_and_Load.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace CLass_Practice
+{
+    public class Save_and_Load<T>
+    {
+        public Save_and_Load() { }
+
+        public void Save(string s, T t)
+        {
+            FileStream SaveFile = File.Create(@"..\..\SavedTeamFiles" + s + ".bin");
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(SaveFile, t);
+        }
+
+        public void /*T*/ Load(string s)
+        {
+
+        }
+    }
+}
